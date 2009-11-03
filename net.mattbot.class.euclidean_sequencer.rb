dc37e961b1c983e10cc79bafdff6da541ef65161
Matt Ridenour <mattridenour@gmail.com>
2009-11-02 19:39:55 -0800

    added MIT license stuff

--- net.mattbot.class.euclidean_sequencer.rb
+++ net.mattbot.class.euclidean_sequencer.rb
@@ -1,24 +1,44 @@
-# net.mattbot.class.euclidean_sequencer.rb
-# by Matt Ridenour (mattridenour@gmail.com)
-# Created on Sun Oct 26 2008
-#
-# A class for spaceing a number of ones over a range of zeros as evenly as possible using the Euclidean algorithm.
-#
-# For more information on using the Euclidean algorithm in the context of music, please read:
-# The Euclidean Algorithm Generates Traditional Musical Rhythms by Godfried Toussaint
-# http://cgm.cs.mcgill.ca/~godfried/publications/banff.pdf
-#
-# Special thanks to Wesen for bringing Euclidean sequencing to my attention and forging the way using Lisp:
-# http://ruinwesen.com/blog?id=216
-#
-# Usage:
-# require "/path/to/net.mattbot.class.euclidean_sequencer.rb"
-# pulses = 8
-# hits = 5
-# thisEC = EuclideanSequencer.new()
-# p thisEC.generate(hits,pulses)
-#
-# Oontz!
+=begin
+net.mattbot.class.euclidean_sequencer.rb
+MIT License
+
+Copyright (c) 2008 Matt Ridenour
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+net.mattbot.class.euclidean_sequencer.rb
+by Matt Ridenour (mattridenour@gmail.com)
+Created on Sun Oct 26 2008
+
+A class for spaceing a number of ones over a range of zeros as evenly as possible using the Euclidean algorithm.
+
+For more information on using the Euclidean algorithm in the context of music, please read:
+The Euclidean Algorithm Generates Traditional Musical Rhythms by Godfried Toussaint
+http://cgm.cs.mcgill.ca/~godfried/publications/banff.pdf
+
+Special thanks to Wesen for bringing Euclidean sequencing to my attention and forging the way using Lisp:
+http://ruinwesen.com/blog?id=216
+
+Usage:
+require "/path/to/net.mattbot.class.euclidean_sequencer.rb"
+pulses = 8
+hits = 5
+thisEC = EuclideanSequencer.new()
+p thisEC.generate(hits,pulses)
+
+Oontz!
+
+=end
 
 class EuclideanSequencer
   def initialize
